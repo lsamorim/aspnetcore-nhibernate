5d7ca46f6b551763fcff3b9055beb324e7c41f62
lsamorim <lsamorim.it@gmail.com>
2019-02-26 03:18:53 -0300

    First commit

---
+++ src/AspNetNHibernate.API/Entities/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AspNetNHibernate.API.Entities
+{
+    public class Entity
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedAt { get; set; }
+        public virtual string CreatedBy { get; set; }
+        public virtual DateTime UpdatedAt { get; set; }
+        public virtual string UpdatedBy { get; set; }
+    }
+}
---
+++ src/AspNetNHibernate.API/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using AspNetNHibernate.API.Entities;
+using HibernatingRhinos.Profiler.Appender.NHibernate;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+
+namespace AspNetNHibernate.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var cfg = new Configuration();
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = Configuration.GetConnectionString("DutchConnectionString");
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2012Dialect>();
+                x.LogSqlInConsole = true;
+            });
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            var sessionFactory = cfg.BuildSessionFactory();
+            using (var session = sessionFactory.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    var products = session.Query<Products>()
+                        .Where(x => x.Price < 1000)
+                        .OrderBy(x => x.Title)
+                        .Select(x => x);
+
+                    foreach(var p in products)
+                    {
+                        Console.WriteLine($"{p.Id} - {p.Title}");
+                    }
+                    
+
+                    tx.Commit();
+                }
+            }
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
